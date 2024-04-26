7768fd844553a98434fdef0bc02c7225860523de
Phuwarin <m.phuwarin@gmail.com>
2024-04-27 04:44:48 +0700

    Update title of first column

--- src/app/todo-list/page.tsx
+++ src/app/todo-list/page.tsx
@@ -54,6 +54,7 @@ export default function AutoDeleteTodoList() {
   return (
     <div className="grid grid-cols-3 gap-4 p-4 ">
       <div className="col-span-1 bg-custom-yellow p-4 rounded-lg">
+        <h2 className="text-center font-bold mb-2">Todo Items</h2>
         {todoItems?.map(
           (item) =>
             !item.isSelected && (
